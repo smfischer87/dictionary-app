e101d2b60ecfa47d4b78cf1332a56418dc7aea62
smfischer87 <smbansmer@gmail.com>
2021-09-14 13:20:25 -0700

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         Dictionary App
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Susan Fischer</footer>
+      </div>
     </div>
   );
 }
