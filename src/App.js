7561e93c2383d4da56789b180dc43f109637ca62
smfischer87 <smbansmer@gmail.com>
2021-09-14 12:23:38 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Dictionary App
+      </header>
+    </div>
+  );
+}
+
+export default App;
